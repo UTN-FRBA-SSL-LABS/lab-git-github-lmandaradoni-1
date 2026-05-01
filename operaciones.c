cec7cd6de01f65453e6c44f40fe95d9d76fc3748
Luca Mandaradoni <lmandaradoni@frba.utn.edu.ar>
2026-05-01 20:17:11 +0000

    Agregue comentario en funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -15,7 +15,7 @@ int restar(int a, int b) {
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
 int multiplicar(int a, int b) {
-    
+    // Con el * la variable 'a' va a multiplicarse con 'b'
     return a* b;
 }
 
